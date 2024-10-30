4e0e539c5342c133a7b2a20d3afd3bb25fa668f2
Hunter Antal <hunterantalgithub@gmail.com>
2024-10-30 17:15:54 -0400

    Added TIme

--- src/bot/Bot.java
+++ src/bot/Bot.java
@@ -1,4 +1,8 @@
 package bot;
+import java.util.ArrayList;
+import java.time.*;
+
+
 
 public class Bot {
     public static void main(String[] args) {
@@ -24,16 +28,36 @@ public class Bot {
         EnumTest seventhDay = new EnumTest(Day.SUNDAY);
         seventhDay.tellItLikeItIs();
 
-        switch (name){
-            case "1":
-                System.out.println("Its 1");
-                break;
-            case "Hunter":
-                System.out.println("Its Hunter");
-                break;
-            default:
-                System.out.println("Non of the above");
+        switch (name) {
+            case "1" -> System.out.println("Its 1");
+            case "Hunter" -> System.out.println("Its Hunter");
+            default -> System.out.println("Non of the above");
         }
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(1);
+        numbers.forEach( (n) -> { System.out.println(n); } );
+
+        LocalDate today = LocalDate.now();
+        System.out.println("Today : " + today);
+
+        LocalTime thisTime = LocalTime.now();
+        System.out.println("This time : " + thisTime);
+
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        System.out.println("Current Time : " + currentDateTime);
+
+        LocalDate someDay = LocalDate.of(2025, Month.JUNE, 12);
+        System.out.println("Someday : " + someDay);
+
+        LocalTime someTime = LocalTime.of(23, 53);
+        System.out.println("Sometime : " + someTime);
+
+        LocalDateTime otherDateTime = LocalDateTime.of(2021, Month.MARCH, 4, 10,51,44);
+        System.out.println("Other Date Time : " + otherDateTime);
+
     }
     public static void addExclamationPoint(String s){
         System.out.println(s + "!");
